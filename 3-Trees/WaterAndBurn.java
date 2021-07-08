5da0b2610623c1d55e001a8f1a61cf04302390d7
japneetbhatia <japneetk1111@gmail.com>
2021-07-08 23:09:32 +0530

    graph

--- 3-Trees/WaterAndBurn.java
+++ 3-Trees/WaterAndBurn.java
@@ -1,37 +1,43 @@
-public void printAtDepthK(TreeNode root, int time, TreeNode blockNode, ArrayList<ArrayList<Integer>> ans) {
-        if (root == null || root == blockNode)
-            return;
-
-        if (time == ans.size()) {
-            ans.add(new ArrayList<>());
-        }
-        ans.get(time).add(root.val);
-
-        printAtDepthK(root.left,time + 1, blockNode, ans);
-        printAtDepthK(root.right, time + 1, blockNode, ans);
-    }
-    public int waterAndBurn(TreeNode root, int target, ArrayList<ArrayList<Integer>> ans, HashSet<TreeNode> water) {
-        if(root == null)
+// -1 : did we gett the target node, -2 : fire will not reach that node, t > 0 :
+    // fire will reach with time t.
+    public static int burningTreeWithWater(TreeNode root, int target, ArrayList<ArrayList<Integer>> ans,
+            HashSet<Integer> water) {
+        if (root == null)
             return -1;
+        if (root.val == target) {
+            if (!water.contains(root.val)) {
+                kdown(root, 0, null, ans);
+                return 1;
+            } else
+                return -2;
+        }
 
-        if(water.containsKey(root))   
-            return -2; 
-        
-        if(root.val == target && !water.containsKey(root)) {
-            printAtDepthK(root,0,null,ans);
-            return 1;
+        int ld = burningTreeWithWater(root.left, target, ans, water);
+        if (ld > 0) {
+            if (!water.contains(root.val)) {
+                kdown(root, ld, root.left, ans);
+                return ld + 1;
+            }
+            return -2;
         }
-        
-        int  ld = waterAndBurn(root.left, target,ans); 
-        if(ld != -1) {
-            printAtDepthK(root,ld,root.left,ans);
-            return ld + 1;
+        if (ld == -2)
+            return -2;
+
+        int rd = burningTreeWithWater(root.right, target, ans, water);
+        if (rd > 0) {
+            if (!water.contains(root.val)) {
+                kdown(root, rd, root.right, ans);
+                return rd + 1;
+            }
+            return -2;
         }
-        
-        int  rd = waterAndBurn(root.right, target, k, ans); 
-        if(rd != -1) {
-            printAtDepthK(root,rd,root.right,ans);
-            return rd + 1;
-        }
+        if (rd == -2)
+            return -2;
+
         return -1;
+    }
+
+    public static void burningTree(TreeNode root, int target) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        burningTree(root, target, ans);
     }
