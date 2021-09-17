ab22ffed6d95e500c06290ae12d92c1a823f49e6
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2021-09-17 23:15:29 +0530

    arrays and strings

--- 6-arraysAndStrings/l001.java
+++ 6-arraysAndStrings/l001.java
@@ -72,6 +72,77 @@ public class l001 {
         }
         return res;
     }
+
+    // leetcode 169. https://leetcode.com/problems/majority-element/ 
+    public int majorityElement1(int[] nums) {
+        int val = nums[0];
+        int count = 1;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == val) {
+                count++;
+            } else {
+                if(count > 0) {
+                    // pairing for distinct elements
+                    count--;
+                } else {
+                    // reset
+                    val = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return val;
+    }
+
+    // leetcode 229. https://leetcode.com/problems/majority-element-ii/
+    private boolean isMajority(int[] arr, int val) {
+        int count = 0;
+        for(int ele : arr) {
+            if(val == ele)
+                count++;
+        }
+        return count > arr.length / 3;
+    }
+
+    public List<Integer> majorityElement(int[] nums) {
+        int val1 = nums[0];
+        int count1 = 1;
+        int val2 = nums[0];
+        int count2 = 0;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == val1) {
+                count1++;
+            }  else if(nums[i] == val2) {
+                count2++;
+            } else {
+                // may be priplet, or a fresh start
+                if(count1 == 0) {
+                    val1 = nums[i];
+                    count1 = 1;
+                } else if(count2 == 0) {
+                    val2 = nums[i];
+                    count2 = 1;
+                } else {
+                    count1--;
+                    count2--;
+                }
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+
+        // check if val1 and val2 present in majority or not
+        if(isMajority(nums, val1)) {
+            res.add(val1);
+        } 
+
+        if(val1 != val2 && isMajority(nums, val2)) {
+            res.add(val2);
+        }
+        return res;
+    }
     
     // 556. Next Greater Element III
     public int nextGreaterElement(int n) {
