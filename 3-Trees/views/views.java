518d5e35134907322235eda5a8fb4db9bd74c1c8
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2022-01-05 20:37:31 +0530

    trees

--- 3-Trees/views/views.java
+++ 3-Trees/views/views.java
@@ -23,5 +23,33 @@ public class views {
         for(var list : ans) 
             System.out.print(list);
     }
+
+    //gfg
+    class Tree {
+    public static ArrayList <Integer> diagonalSum(Node root) 
+    {
+        // code here.
+        LinkedList<Node> que = new LinkedList<>();
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        que.addLast(root);
+        while(que.size() != 0) {
+            int size = que.size();
+            int smallAns = 0;
+            while(size-- > 0) {
+                Node rm = que.removeFirst();
+                
+                while(rm != null) {
+                    smallAns += rm.data;
+                    if(rm.left != null)
+                        que.add(rm.left);
+                    rm = rm.right;
+                }
+            }
+            ans.add(smallAns);
+        }
+        return ans;
+    }
+}
 }
 
